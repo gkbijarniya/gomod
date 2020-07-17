6d84be0c3c76501503619fe6e0f4249cb01cf4ca
Gopal Krishan Bijarniya <GBijarniya@walmartlabs.com>
2020-07-17 22:36:02 +0530

    update for v2.0.0

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // a simple update for version change
 // a simple update for version v1.0.1
+// a simple update for version v2.0.0
 func hello() {
-	fmt.Println("Hello from hello.v1")
+	fmt.Println("Hello from hello.v2")
 }
